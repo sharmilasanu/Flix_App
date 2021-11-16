ed4b3ebbbbe9021deca87aac14667a331225b9eb
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2021-11-16 17:02:36 +0100

    feat : Password hashing for authentication

--- models.js
+++ models.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 let movieSchema = mongoose.Schema({
+
     Title : {type: String, required:true},
     Desc : {type: String, required:true},
     Genre : {
@@ -15,13 +18,20 @@ let movieSchema = mongoose.Schema({
   Featured: Boolean
 });
 let userSchema = mongoose.Schema({
-
-    UserName : {type:String, required: true},
-    Password : {type:String,required:true},
-    Email : {type:String,required:true},
-    Birthday : Date,
-    FavoriteMovies : [{ type: mongoose.Schema.Types.ObjectId,ref :'Movie'}]
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 //  This will create collections called “db.movies” and “db.users” within the MongoDB database
 let Movie = mongoose.model('Movie',movieSchema);
