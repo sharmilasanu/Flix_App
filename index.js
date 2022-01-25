914b1ea9d7362ccbfcba107c967b4201a51a4caf
sharmilasanu <79763325+sharmilasanu@users.noreply.github.com>
2022-01-25 18:15:50 +0100

    Cors - Allow origin  : sharmismyflix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(passport.initialize());
 
 const cors = require('cors');
 // will allow only certain regions to access this api
-let allowedOrigins = ['http://localhost:8080', 'https://sharmismyflix.herokuapp.com','http://localhost:1234','http://localhost:4200','https://sharmilasanu.github.io'];
+let allowedOrigins = ['http://localhost:8080', 'https://sharmismyflix.herokuapp.com','http://localhost:1234','http://localhost:4200','https://sharmilasanu.github.io' , 'https://sharmismyflix.netlify.app/'];
 
 app.use(cors({
   origin: (origin, callback) => {
